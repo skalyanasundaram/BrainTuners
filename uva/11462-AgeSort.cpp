e9e90539cdd4bf6aef9c479eb41cbd87fec897de
Kalyan <s.kalyanasundaram@gmail.com>
2013-10-15 20:30:05 +0530

    Removed some unused code..

--- uva/11462-AgeSort.cpp
+++ uva/11462-AgeSort.cpp
@@ -1,53 +1,8 @@
 #include <iostream>
 #include <string>
-//#include <sstream>
 #include <vector>
 
 using namespace std;
-
-void shift_down(vector<unsigned short> &arr, long cur, unsigned long size)
-{
-    long left = cur * 2 + 1,
-        right = cur * 2 + 2,
-        max = cur;
-
-    if (left < size && arr[left] > arr[max])
-    {
-        max = left;
-    }
-    if (right < size && arr[right] > arr[max])
-    {
-        max = right;
-    }
-
-    if (max != cur)
-    {
-        unsigned short t = arr[max];
-        arr[max] = arr[cur];
-        arr[cur] = t;
-        shift_down(arr, max, size);
-    }
-}
-
-void max_heapify(vector<unsigned short> &arr, unsigned long size)
-{
-    for (long p = (size / 2) - 1; p >= 0; p--)
-    {
-        shift_down(arr, p, size);
-    }
-}
-
-void heap_sort(vector<unsigned short> &arr, unsigned long size)
-{
-    max_heapify(arr, size);
-    while (size > 1)
-    {
-        unsigned long t = arr[0];
-        arr[0] = arr[size - 1];
-        arr[size - 1] = t;
-        max_heapify(arr, --size);
-    }
-}
 
 int main()
 {
